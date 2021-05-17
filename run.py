6e7ce143c04ea151dfc5cd54c78b7df2d095a918
Jakub Krakowiak <kubamrga999kk@onet.pl>
2021-05-17 17:37:01 +0200

    improved screens

--- run.py
+++ run.py
@@ -4,11 +4,9 @@ import readkeys
 import os
 
 
-
-
 class Game:
     def __init__(self):
-        self.time = 1000
+        self.time = 100
         self.threat = 50
 
     def add_time(self, time):
@@ -31,9 +29,12 @@ class Player(Person):
 
 
 class Guard(Person):
-    def __init__(self, name, lastname):
-        super().__init__(name, lastname)
+    def __init__(self):
+        super().__init__(names.get_first_name(gender='male'), names.get_last_name())
         self.power = random.randint(1, 60)
+
+    def introduce(self):
+        return self.name + " " + self.lastname
 
 
 class Lock:
@@ -48,6 +49,58 @@ class Gun:
 
 
 class View:
+    Part = Game()
+    Guard1 = Guard()
+    Guard2 = Guard()
+    Guard3 = Guard()
+    Civil1 = Guard()
+    desc = [
+        "Jesteś agentem 123 i musisz ukraść superbroń z bazy złej organizacji, to jana misja dlatego nie możesz dać się zauważyć.",
+        "Widzisz pokój kontrolny z kamerami.",
+        "Widzisz strażnika " + Guard1.introduce() + " w pokoju z kamerami.",
+        "Na końcu korytarza widzisz stalowe zamknięte drzwi",
+        "Widzisz dwóch rozmawiających ze sobą strażników  " + Guard2.introduce() + " i " + Guard3.introduce(),
+        "Zauważa cię cywilny pracownik (" + Civil1.introduce() + ").",
+        "Widzisz zbrojownię co robisz?",
+        "Wchodzisz do dużego magazynu. Po drugiej stronie pomieszczenia widzisz przełącznik przeciwpożarowy.",
+        "Przed twoimi oczami pojawia się się wielki sejf z zamkiem elektronicznym.",
+        "Znajdujesz w sejfie tajną broń zabierasz ją i uciekasz włazem"
+        ]
+    picks = [
+        ["Wejdź do budynku przez wentylację. (zabiera 5 czasu daje 5% wykrycia)", 1, 2, -5, 5, 0],
+        ["Wejdź do budynku przez okno. ( zabiera 10 czasu daje 10% wykrycia)", 1, 2, 0, 0, 0],
+        ["Wejdź frontowymi drzwiami. ( zabiera 1 czasu daje 100% wykrycia)", 1, 2, -1, 100, 0],
+        ["Wyłącz kamery. (zabiera 10 czasu odejmuje 15% wykrycia)", 2, 3, -10, 15, 0],
+        ["Idź dalej korytarzem. (zabiera 1 czasu)", 2, 4, -1, 0, 0],
+        ["Strzelasz do strażnika.(zabiera 1 czasu, zabiera 1 pocisk)", 3, 4, -1, 0, -1],
+        ["Ogłuszasz strażnika.(zabiera 5 czasu)", 3, 4, -5, 0, 0],
+        ["Otwierasz zamek wytrychem.(zabiera 5 czasu)", 4, 5, -5, 0, 0],
+        ["Strzelasz do zamka. (zabiera 1 czasu, zwiększa wykrycie o 3%, zabiera 1 ammo)", 4, 5, -1, 3, -1],
+        ["Przekradasz się obok nich. (zabiera 5 czasu)", 5, 6, -5, 0, 0],
+        ["Strzelasz do nich. (zabiera 1 czasu, zabiera 2 ammo)", 5, 6, -1, 0, -2],
+        ["Bijesz się z nimi. (zabiera 2 czasu, podnosi wykrycie o 10%)", 5, 6, -2, 10, 0],
+        ["Uciekasz przed nimi. (tracisz 1 czasu, wykrycie wzrasta)", 6, 7, -1, 10, 0],
+        ["Obezwładniasz cywila. (tracisz 5 czasu)", 6, 7, -5, 0, 0],
+        ["Przeszukujesz ją w poszukiwaniu amunicji. (dodaje x naboi, tracisz 5 czasu)", 7, 8, -5, 0, 2],
+        ["Idziesz dalej korytarzem. (tracisz 1 czasu)", 7, 8, -2, 0, 0],
+        ["Strzelsz w przełącznik odwracając uwagę strażników. (odejmuje 5 czasu, odejmuje 10 wykrycia, odejmuje 1 "
+         "amunicji)", 8, 9, -5, -10, -1],
+        ["Przełączasz czujnik  ręcznie. (odejmuje 10 czasu, odejmuje 10 wykrycia)", 8, 9, -10, -10, 0],
+        ["Ignorujesz czujnik. (odejmuje 1 czasu)", 8, 9, -1, 0, 0],
+        ["Hakujesz drzwi. (zabiera 5 czasu daje 5 wykrycia)", 9, 10, -5, 5, 0],
+        ["Szukasz karty dostępu. (zabiera 10 czasu, daje 2 wykrycia)", 9, 10, -10, 2, 0],
+        ["Uciekasz korytarzem w lewo.", 10, 11, 0, 0, 0],
+        ["Uciekasz korytarzem w prawo.", 10, 11, 0, 0, 0]
+    ]
+
+    def text_wrapper(self, left, right, fill_char):
+        try:
+            columns, rows = os.get_terminal_size(0)
+        except OSError:
+            columns, rows = os.get_terminal_size(1)
+        return str(left) + str(fill_char)*(columns - len(left) - len(right)) + str(right)
+
+
     def clearConsole(self):
         os.system('cls')
 
@@ -64,6 +117,8 @@ class View:
             "| Każdy wybór ma znaczenie!                                                                               |")
         print(
             "| Wybieraj mądrze.                                                                                        |")
+        print(
+            "| Wciśnij Spacje.                                                                                         |")
         print(
             "|---------------------------------------------------------------------------------------------------------|")
         print("")
@@ -86,6 +141,8 @@ class View:
             self.welcome()
         elif ch == "2":
             self.rules()
+        elif ch == "0":
+            return
 
     def rules(self):
         self.clearConsole()
@@ -120,13 +177,34 @@ class View:
             "|-----------------------------------------------------------------------------------------------------------------------------------------------------------------------------|")
         print("")
 
-    def action_pick(self, desc, picks, stage):
+    def action_pick(self, stage):
         self.clearConsole()
-        print(desc[stage-1])
-        for p in picks:
+        print(self.desc[stage - 1])
+        i = 0
+        tab = []
+        for p in self.picks:
             if p[1] == stage:
-                print(p[0])
+                tab.append([])
+                tab[i].append(p[2])
+                tab[i].append(p[3])
+                tab[i].append(p[4])
+                tab[i].append(p[5])
+                i += 1
+                print(str(i) + ". " + p[0])
 
+        ch = readkeys.getch()
+
+        if stage == 10:
+            self.gameover_win()
+        else:
+            for t in range(1, len(tab) + 1):
+                if ch == str(t):
+                    self.Part.add_time(tab[t - 1][1])
+                    self.Part.add_threat(tab[t - 1][2])
+                    if self.Part.time <= 0 or self.Part.threat >= 100:
+                        self.gameover_lost(stage)
+                    else:
+                        self.action_pick(tab[t - 1][0])
 
     def gameover_lost(self, stage):
         self.clearConsole()
@@ -134,12 +212,13 @@ class View:
         print(" Zostałeś złapany!")
         print(" Twój wynik to: ", stage)
 
-    def gameover_win(self, ammo, time, threat):
+    def gameover_win(self):
         self.clearConsole()
         print("")
         print(" Gratulacje!")
         print(" Udało Ci się wykraść niebezpieczną broń.")
-        print(" Twój wynik to: amunicja - ", ammo, "", "|", "", "wykrycie - ", threat, "", "|", "", "", "czas - ", time)
+        print(" Twój wynik to: amunicja - ", "2", "", "|", "", "wykrycie - ", self.Part.threat, "", "|", "", "",
+              "czas - ", self.Part.time)
 
     def weapon_pick(self):
         self.clearConsole()
@@ -147,28 +226,61 @@ class View:
         print(
             "|---------------------------------------------------------------------------------------------------------|")
         print(
-            "| Wybierz Broń                                                                                            |")
+            "| Wybierz Broń                                                                                            |\n")
+        print(
+            "| 1. Plainsrider Bow                                                                                      |")
+        print(
+            "| 2. USP Compact Tactical                                                                                 |")
+        print(
+            "| 3. Vulcan Minigun                                                                                       |")
         print(
             "| Wybieraj mądrze.                                                                                        |")
         print(
             "|---------------------------------------------------------------------------------------------------------|")
         print("")
+        ch = readkeys.getch()
+        if ch == "1":
+            self.Weapon = Gun('Plainsrider Bow', 3)
+            self.Part.add_threat(-20)
+        elif ch == "2":
+            self.class_pick('USP Compact Tactical', 10)
+            self.Part.add_threat(0)
+
+        elif ch == "3":
+            self.class_pick('Vulcan Minigun', 20)
+            self.Part.add_threat(25)
+
+    def class_pick(self):
+        self.clearConsole()
+        print("")
+        print(
+            "|---------------------------------------------------------------------------------------------------------|")
+        print(
+            "| Wybierz Klasę                                                                                           |")
+        print(
+            "| Wybieraj mądrze.                                                                                        |")
+        print(
+            "|---------------------------------------------------------------------------------------------------------|")
+        print("")
+        ch = readkeys.getch()
+        if ch == "1":
+            self.Character = Player("Jakub", "Wick", 0)
+            self.action_pick(1)
+        elif ch == "2":
+            self.Character = Player("Jakub", "Wick", 1)
+            self.action_pick(1)
+        elif ch == "3":
+            self.Character = Player("Jakub", "Wick", 2)
+            self.action_pick(1)
+
+
+
 
 
 def main():
     print('elo')
-    Screen = View()
-    desc = ["Opis1", "Opis2"]
-    desc[1]
-    picks = [
-        ["Wejdż do budynku przez wentylację(zabiera 5 czasu daje 5% wykrycia)", 1, 2, -5, 5, 0],
-        ["Wejdź do budynku przez okno( zabiera 10 czas daje 10% wykrycia)", 1, 2, 0, 0, 0],
-        ["Wejdz frontowymi drzwiami( zabiera 1 czasu daje 100% wykrycia)", 1, 2, -1, 100, 0],
-        ["dsadsada(zabiera 5 czasu daje 5% wykrycia)", 2, 3, -5, 5, 0],
-        ["Wdasdaso( zabiera 10 czas daje 10% wykrycia)", 2, 3, 0, 0, 0],
-        ["Wdasdasd( zabiera 1 czasu daje 100% wykrycia)", 2, 3, -1, 100, 0]
-    ]
-    Screen.action_pick(desc, picks, 2)
+    #Screen = View()
+    #Screen.start()
 
     input()
 
